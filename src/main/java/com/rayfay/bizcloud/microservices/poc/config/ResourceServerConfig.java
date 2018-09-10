72a7c9b8df9212a23a5df19eeeee6825b24267c3
fu shen <scanf1983@gmail.com>
2018-09-10 08:43:49 +0800

    swagger

--- src/main/java/com/rayfay/bizcloud/microservices/poc/config/ResourceServerConfig.java
+++ src/main/java/com/rayfay/bizcloud/microservices/poc/config/ResourceServerConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests().antMatchers("/v2/**", "/swagger**", "/images/**", "/configuration/**").permitAll();
+        http.authorizeRequests().antMatchers("/v2/**", "/swagger**", "/images/**", "/configuration/**").permitAll();
 //        http.antMatcher("/**").authorizeRequests().anyRequest().authenticated();
     }
 }
